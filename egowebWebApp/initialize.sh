83f0a5ba5006fabb11025b66b6277c34126dfa2d
sdrulea <sdrulea@rand.org>
2014-09-30 18:07:26 -0700

    Create main.php from *.bak and added shebang line to initialize.sh.

--- egowebWebApp/initialize.sh
+++ egowebWebApp/initialize.sh
@@ -1,4 +1,6 @@
+#!/bin/sh
 mkdir -p assets
 chmod 777 assets
 mkdir -p ${PWD}/protected/runtime
 chmod 777 ${PWD}/protected/runtime
+cp -n ${PWD}/protected/config/main.php.bak ${PWD}/protected/config/main.php
