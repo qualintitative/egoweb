24fd85c6ab7dac8ff33b7457bc0a1e71729ee15b
sdrulea <sdrulea@rand.org>
2014-10-22 18:06:46 -0700

    Encrypt notes.notes and affected model.

---
+++ egowebWebApp/protected/migrations/m141023_005646_encrypt_notes.php
@@ -0,0 +1,48 @@
+<?php
+require_once(dirname(__FILE__)."/../extensions/custy.php");
+
+
+class m141023_005646_encrypt_notes extends CDbMigration
+{
+    public function up()
+    {
+        $transaction=$this->getDbConnection()->beginTransaction();
+
+        try
+        {
+            $cmd = $this->getDbConnection()->createCommand( "SELECT * FROM notes");
+            $rows = $cmd->queryAll();
+
+            foreach( $rows as $row ){
+                if( strlen( trim( $row["notes"] ) ) > 0 ){
+                    $encrypted = encrypt( $row["notes"] );
+                    $this->update( 'notes', array( 'notes'=>$encrypted ), 'id='.$row["id"] );
+                }
+            }
+            $transaction->commit();
+        }
+        catch(Exception $e)
+        {
+            echo "Exception: ".$e->getMessage()."\n";
+            $transaction->rollback();
+            return false;
+        }
+    }
+
+	public function down()
+	{
+		echo "m141023_005646_encrypt_notes does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
