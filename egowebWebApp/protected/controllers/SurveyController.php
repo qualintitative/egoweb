0655c6b5a55541690e4b44fff68b4a75497b48d4
sdrulea <sdrulea@rand.org>
2015-02-20 18:05:31 -0800

    Removed SurveyController encrypting & decrypting function in favor of standard egoweb encrypt() and decrypt() functions. Removed need for "password" parameter in encrypted links.

--- egowebWebApp/protected/controllers/SurveyController.php
+++ egowebWebApp/protected/controllers/SurveyController.php
@@ -9,38 +9,14 @@
 Yii::import('ext.httpclient.*');
 
 class SurveyController extends Controller {
-    private $privateKey;
-    private $publicKey;
-    private $iv_size;
-
-    public function init(){
-        $this->publicKey =  openssl_get_publickey( Yii::app()->params['surveyPublicKey'] );
-
-        if( empty( $this->publicKey ) ){
-            throw new \Exception('Public key invalid:' . openssl_error_string());
-        }
-
-        $this->privateKey = openssl_get_privatekey( Yii::app()->params['surveyPrivateKey'], Yii::app()->params['surveyEncryptionPassword'] );
-
-        if( empty( $this->publicKey ) ){
-            throw new \Exception('Private key invalid:' . openssl_error_string());
-        }
-
-        $this->iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
-    }
-
+    
     public function actionIndex(){
         if( !isset( $_REQUEST['payload'] ) ){
             $msg = 'Missing payload parameter';
             return ApiController::sendResponse( 419, $msg );
         }
 
-        if( !isset( $_REQUEST['password'] ) ){
-            $msg = 'Missing payload parameter';
-            return ApiController::sendResponse( 419, $msg );
-        }
-
-        return $this->receive( $_REQUEST['payload'], $_REQUEST['password'] );
+        return $this->receive( $_REQUEST['payload'] );
     }
 
     /**
@@ -101,14 +77,9 @@ class SurveyController extends Controller {
      * @param string $payload
      * @return array
      */
-    public function receive( $payload, &$password ){
-        $password = $this->asymDecrypt( base64_decode( html_entity_decode( $password ) ) );
-        if( !isset( $password ) ){
-            return ApiController::sendResponse( 422, 'Unable to decrypt password' );
-        }
-
-        $plain = $this->decrypt( $payload, $password );
-        if( !isset( $password ) ){
+    public function receive( $payload ){
+        $plain = decrypt( $payload);
+        if( !isset( $plain ) ){
             return ApiController::sendResponse( 422, 'Unable to decrypt payload' );
         }
 
@@ -186,55 +157,7 @@ class SurveyController extends Controller {
         }elseif( !array_key_exists ( 'redirect_url', $decoded )){
             return ApiController::sendResponse( 500, 'Invalid survey redirect' );
         }
-        $this->redirect( $this->createUrl($decoded->redirect_url) );
-    }
-
-    /**
-     * @param $encrypted
-     * @return mixed
-     */
-    private function asymDecrypt($encrypted){
-        $result = openssl_private_decrypt( $encrypted, $plain, $this->privateKey );
-
-        return $plain;
-    }
-
-    /**
-     * @param $plain
-     * @return mixed
-     */
-    private function asymEncrypt($plain){
-        $res = openssl_public_encrypt($plain, $encrypted, $this->publicKey);
-        return $encrypted;
-    }
-
-    /**
-     * @param $encrypted
-     * @param $key
-     * @return string
-     */
-    private function decrypt($encrypted, $key){
-        $ciphertext_dec = base64_decode($encrypted);
-        # retrieves the IV, iv_size should be created using mcrypt_get_iv_size()
-        $iv_dec = substr($ciphertext_dec, 0, $this->iv_size);
-
-        # retrieves the cipher text (everything except the $iv_size in the front)
-        $ciphertext_dec = substr($ciphertext_dec, $this->iv_size);
-
-        # may remove 00h valued characters from end of plain text
-        $plaintext_dec = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key,$ciphertext_dec, MCRYPT_MODE_CBC, $iv_dec);
-
-        return $plaintext_dec;
-    }
-
-    /**
-     * @param $l
-     * @param string $c
-     * @return string
-     */
-    function mt_rand_str ($l, $c = '0123456789ABCDEF') {
-        for ($s = '', $cl = strlen($c)-1, $i = 0; $i < $l; $s .= $c[mt_rand(0, $cl)], ++$i);
-        return $s;
+        $this->redirect( $this->createUrl( $decoded->redirect_url ) );
     }
 
     /**
@@ -243,45 +166,12 @@ class SurveyController extends Controller {
      */
     public function generateRequestString( $payload ){
 
-        $password = pack('H*', $this->mt_rand_str(64));
-
         $plain = json_encode( $payload );
-        $content = $this->encrypt( $plain, $password );
-        $p = base64_encode( $this->asymEncrypt( $password ) );
+        $content = encrypt( $plain );
 
-        $data = http_build_query( array( 'password' => $p, 'payload' => $content ) );
+        $data = http_build_query( array( 'payload' => $content ) );
 
         return 'http://'.$_SERVER['HTTP_HOST'].'/survey?'.$data;
-    }
-
-    /**
-     * @param $plain
-     * @param $key
-     * @param bool $prependIV
-     * @param string $mode
-     * @return string
-     */
-    public static function encrypt( $plain, $key, $prependIV = true, $mode = MCRYPT_MODE_CBC ){
-        if (empty($plain)){
-            $plain = '';
-        }
-
-        $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, $mode);
-
-        # create a random IV to use with CBC encoding
-        $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
-        # creates a cipher text to keep the text confidential
-        # only suitable for encoded input that never ends with value 00h
-        # (because of default zero padding)
-        $ciphertext = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $plain, $mode, $iv);
-
-        if ($prependIV){
-            # prepend the IV for it to be available for decryption
-            $ciphertext = $iv . $ciphertext;
-        }
-        # encode the resulting cipher text so it can be represented by a string
-
-        return base64_encode($ciphertext);
     }
 
 } 
