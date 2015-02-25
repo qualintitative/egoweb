3370321d6abf2673bb3b3555c1609ddaf10f5db4
David Zhang <bellinghampermaculture@gmail.com>
2015-02-24 20:20:51 -0800

    scroll fix

--- egowebWebApp/protected/views/authoring/_view_question_list.php
+++ egowebWebApp/protected/views/authoring/_view_question_list.php
@@ -1,4 +1,3 @@
-<?php Yii::app()->clientScript->registerCoreScript('jquery.ui'); ?>
 <?php $this->widget('zii.widgets.CListView', array(
 	'dataProvider'=>$dataProvider,
 	'itemView'=>'_view_question',
