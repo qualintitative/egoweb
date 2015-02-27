b669ff43025089e3cad1ba39c9abc46e43aa7226
David Zhang <bellinghampermaculture@gmail.com>
2015-02-26 17:35:37 -0800

    now function

--- egowebWebApp/protected/models/Interview.php
+++ egowebWebApp/protected/models/Interview.php
@@ -379,27 +379,33 @@ class Interview extends CActiveRecord
 			}else{
 				$question = Question::model()->findByAttributes(array('title'=>$qTitle, 'studyId'=>$studyId));
 			}
-			if(!$question || $question->answerType != "DATE")
-				continue;
-			$criteria=new CDbCriteria;
-				if($interviewId != null)
-					$end = " AND interviewId in (". $interviewId. ")";
-				else
-					$end = "";
-			$criteria=array(
-				'condition'=>'questionId = '. $question->id . $end,
-			);
-			$answer = Answer::model()->find($criteria);
-			$timeArray = Question::timeBits($question->timeUnits);
-			$timeFormat = "";
-			if(in_array("BIT_MONTH", $timeArray))
-				$timeFormat = "F ";
-			if(in_array("BIT_DAY", $timeArray))
-				$timeFormat .= "jS ";
-			if(in_array("BIT_YEAR", $timeArray))
-				$timeFormat .= ", Y";
-			if(in_array("BIT_HOUR", $timeArray))
-				$timeFormat .= "h:i A";
+			if(strtolower($qTitle) == "now"){
+				$answer = new Answer;
+				$answer->value = "now";
+				$timeFormat = "F jS, Y";
+			}else{
+				if(!$question || $question->answerType != "DATE")
+					continue;
+					$criteria=new CDbCriteria;
+					if($interviewId != null)
+						$end = " AND interviewId in (". $interviewId. ")";
+					else
+						$end = "";
+					$criteria=array(
+						'condition'=>'questionId = '. $question->id . $end,
+					);
+					$answer = Answer::model()->find($criteria);
+					$timeArray = Question::timeBits($question->timeUnits);
+					$timeFormat = "";
+					if(in_array("BIT_MONTH", $timeArray))
+						$timeFormat = "F ";
+					if(in_array("BIT_DAY", $timeArray))
+						$timeFormat .= "jS ";
+					if(in_array("BIT_YEAR", $timeArray))
+						$timeFormat .= ", Y";
+					if(in_array("BIT_HOUR", $timeArray))
+						$timeFormat .= "h:i A";
+			}
 			$newDate = date($timeFormat, strtotime($answer->value . " " .$amount . " " . $period));
 			$string =  str_replace("<DATE ".$date." />", $newDate, $string);
 		}
