d367c5f074ad437e884cc0ea4ee4fc1946c831a3
David Zhang <bellinghampermaculture@gmail.com>
2015-03-05 02:46:13 -0800

    date fix

--- egowebWebApp/protected/views/interviewing/_form_date.php
+++ egowebWebApp/protected/views/interviewing/_form_date.php
@@ -15,7 +15,7 @@ jQuery('.time-".$array_id."').change(function() {
         date = date + $(item + '#year').val() + ' ';
     if(typeof $(item + '#hour').val() != 'undefined'){
         date = date + $(item + '#hour').val() + ':';
-        date = date + $(item + '#minute').val() ? $(item + '#minute').val() : '00';
+        date = date + ($(item + '#minute').val() ? $(item + '#minute').val() : '00');
         date = date + $(item + '#ampm:checked').val();
     }
     $('#Answer_".$array_id."_value').val(date);
