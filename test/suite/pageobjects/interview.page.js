9ed1547c96f4a96ee78c42d72bc9cacbaaa110e7
Amin, Alerk <aamin@rand.org>
2016-09-02 11:36:48 -0700

    initial checkin of test suite, including config, login, basic_fields

---
+++ test/suite/pageobjects/interview.page.js
@@ -0,0 +1,120 @@
+// login.page.js
+var Page = require('./page')
+
+var IwPage = Object.create(Page, {
+    /**
+     * define elements
+     */
+    form: { get: function () { return browser.element('#answerForm'); }},
+    startInterviewLink: { get: function () { return browser.element('=Start new interview'); }},
+    nextButton: { get: function () { return browser.element('button.orangebutton=Next'); }},
+    backButton: { get: function () { return browser.element('button.graybutton=Back'); }},
+    dkLabel: { get: function() { return browser.element('label=Don\'t Know')}},
+    rfLabel: { get: function() { return browser.element('label=Refuse')}},
+    questionTitle: { get: function() { return browser.element('span#questionTitle')}},
+
+    // EGO ID of current case in survey
+    ewid: {
+        value: 0,
+        enumerable: false,
+        writable: true,
+        configurable : false
+    },
+
+    // fake data to use in survey questions
+    fieldValues: {
+        value: {},
+        enumerable: false,
+        writable: true,
+        configurable: false
+    },
+
+    /**
+     * define or overwrite page methods
+     */
+    inputField: { value: function(id) {
+        return browser.element('input#Answer_'+id+'_VALUE');
+    }},
+
+    specificQuestionTitle: { value: function(question) {
+        return browser.element('span#questionTitle='+question);
+    }},
+
+    goBackToQuestion: { value: function(question) {
+        // keep hitting "back" button until we get to the question
+        // TODO - put some "maximum" in this loop, and fail if it doesn't find the question
+        while ((foo = IwPage.questionTitle.getText()) != question) {
+            this.back();
+        }
+        this.pause();
+    }},
+
+    goForwardToQuestion: { value: function(question) {
+        // keep hitting "next" button until we get to the question
+        // TODO - put some "maximum" in this loop, and fail if it doesn't find the question
+        while ((curfield = IwPage.questionTitle.getText()) != question) {
+            if (curfield in this.fieldValues) {
+                let fv = this.fieldValues[curfield];
+                switch (fv.type) {
+                    case 'input':
+                        // simple text filed
+                        this.inputField(fv.field).setValue(fv.value);
+                        break;
+                }
+            }
+            this.next();
+        }
+    }},
+
+    next: { value: function() {
+        this.nextButton.waitForVisible(browser.options.egoweb.waitTime);
+        this.nextButton.click();
+        this.pause();
+    }},
+
+    back: { value: function() {
+        this.backButton.waitForVisible(browser.options.egoweb.waitTime);
+        this.backButton.click();
+        this.pause();
+    }},
+
+    pause: { value: function() {
+        browser.pause(browser.options.egoweb.pauseTime);
+    }},
+
+    openInterview: { value: function(interview) {
+        this.open('interview');
+
+        browser.element("="+interview).click();
+
+        // search existing IDs to find max
+        browser.element("table.items").waitForExist(browser.options.egoweb.waitTime);
+        ids = browser.elements("table.items tr td");
+
+        max = 0;
+        ids.value.forEach(function(el) {
+            val = parseInt(browser.elementIdText(el.ELEMENT).value);
+            if (val > max) {
+                max = val;
+            }
+        });
+
+        // pick a random ID higher than max, in case multiple tests/threads are running simultaneously
+        newid = max + 1 + Math.floor(Math.random() * 100);
+
+        // save ID
+        this.ewid = newid;
+
+        // start interview
+        this.startInterviewLink.click();
+
+        return this;
+    }},
+
+    submit: { value: function() {
+        this.form.submitForm();
+        return this;
+    } }
+});
+
+module.exports = IwPage;
