31d9cc0b02ee00d4c7c942bac686424a3f7b1efc
Zhang <bellinghampermaculture@gmail.com>
2018-04-28 05:01:26 -0700

    sync fix 4

--- app/protected/views/importExport/_interviews.php
+++ app/protected/views/importExport/_interviews.php
@@ -10,7 +10,7 @@ Include Response Data<br>
             $completed = "INCOMPLETE";
         echo "<div class='multiRow' style='width:200px;text-align:left'>".CHtml::checkbox('export[]', false,array('value'=>$interview->id)). " " . Interview::getEgoId($interview->id)."</div>";
         echo "<div class='multiRow' style='width:120px'>".$completed."</div>";
-        echo "<div class='multiRow'>".CHtml::button('Review',array('submit'=>$this->createUrl('/interviewing/'.$study->id.'?interviewId='.$interview->id)))."</div>";
+        echo "<div class='multiRow'>".CHtml::button('Review',array('onclick'=>"document.location='".$this->createUrl('/interview/'.$study->id.'/'.$interview->id . "#/page/0'")))."</div>";
         echo "<div class='multiRow'>".CHtml::button('Visualize',array('submit'=>$this->createUrl('/data/visualize?expressionId=&interviewId='.$interview->id)))."</div>";
         echo "<br style='clear:both'>";
     }
