a416352fd03321ffffef193a2820e4471ffcd3bd
Zhang <bellinghampermaculture@gmail.com>
2018-09-14 17:28:06 -0700

    graph fix 3

--- app/protected/controllers/InterviewController.php
+++ app/protected/controllers/InterviewController.php
@@ -235,7 +235,7 @@ class InterviewController extends Controller
                                 "interviewId" => $i_id,
                                 "expressionId" => $networkExprId,
                                 "studyName" => $s->name,
-                                "params"=> $graphId->params,
+                                "params"=> $nq["NETWORKPARAMS"],
                             );
                         }
                     }
