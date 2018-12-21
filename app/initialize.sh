11b0887e4941d7875f09589d92e1737adee83777
Sherban Drulea <sdrulea@rand.org>
2018-12-20 16:32:30 -0800

    Create console.php in initialize too.

--- app/initialize.sh
+++ app/initialize.sh
@@ -4,3 +4,4 @@ chmod 777 assets
 mkdir -p ${PWD}/protected/runtime
 chmod 777 ${PWD}/protected/runtime
 cp -n ${PWD}/protected/config/main.php.example ${PWD}/protected/config/main.php
+cp -n ${PWD}/protected/config/console.php.example ${PWD}/protected/config/console.php
