042bfb820729bd57f05629b1cd57f09044c9e2d2
Zhang <bellinghampermaculture@gmail.com>
2019-01-17 06:29:45 -0800

    encrypt update

--- app/initialize.sh
+++ app/initialize.sh
@@ -4,3 +4,5 @@ chmod 777 assets
 mkdir -p ${PWD}/protected/runtime
 chmod 777 ${PWD}/protected/runtime
 cp -n ${PWD}/protected/config/main.php.example ${PWD}/protected/config/main.php
+NEW_KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+sed 's/old_key/$NEW_KEY/g' hello.txt
