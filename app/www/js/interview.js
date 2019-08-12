efb40f6b814c73160a8fa46cef78c7be72661cbe
Zhang <dzhangbham@gmail.com>
2019-08-12 12:58:12 -0700

    column fix

--- app/www/js/interview.js
+++ app/www/js/interview.js
@@ -187,7 +187,7 @@ app.controller('interviewController', ['$scope', '$log', '$routeParams', '$sce',
     if ($scope.questions[k].ASKINGSTYLELIST == true)
       $scope.askingStyleList = $scope.questions[k].array_id;
     if ($scope.askingStyleList != false)
-      $scope.fixedWidth = "120px";
+      $scope.fixedWidth = "auto";
     else
       $scope.fixedWidth = "auto";
 
