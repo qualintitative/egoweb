55544c45df38f3c1113af571718849b2fda9bc73
zhangster <dzhang.rand.org>
2022-01-20 14:30:30 -0800

    pagination

--- app/protected/migrations/m210616_191849_update_user_table.php
+++ app/protected/migrations/m210616_191849_update_user_table.php
@@ -7,11 +7,23 @@ class m210616_191849_update_user_table extends CDbMigration
 		$transaction=$this->getDbConnection()->beginTransaction();
         try
         {
+			$table = Yii::app()->db->schema->getTable('user');
+
+			if(!isset($table->columns['password_reset_token']))
 			$this->addColumn('user', 'password_reset_token', 'varchar(255)');
+
+			if(!isset($table->columns['auth_key']))
 			$this->addColumn('user', 'auth_key', 'varchar(255)');
+
+			if(!isset($table->columns['status']))
 			$this->addColumn('user', 'status', 'tinyint');
+			
+			if(!isset($table->columns['created_at']))
 			$this->addColumn('user', 'created_at', 'int');
+
+			if(!isset($table->columns['updated_at']))
 			$this->addColumn('user', 'updated_at', 'int');
+
 			$transaction->commit();
         }
         catch(Exception $e)
