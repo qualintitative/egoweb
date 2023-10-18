c8e696f19853f9b8ab82f8a346f51d46e027c3fe
David Zhang <dzhang@rand.org>
2023-10-18 08:38:41 -0700

    mobile css

--- app/protected/views/layouts/main.php
+++ app/protected/views/layouts/main.php
@@ -20,7 +20,7 @@ AppAsset::register($this);
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
